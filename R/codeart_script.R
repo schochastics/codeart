0e5c584e4d8cd30930462b74a3359c07bde15a23
schochastics <david@schochastics.net>
2020-01-31 10:10:17 +0000

    added

--- R/codeart_script.R
+++ R/codeart_script.R
@@ -4,7 +4,21 @@
 #' @details Inspired by [Eric Davidson](https://github.com/erdavids/Simulated-Code).
 #' @export
 
-codeart_script <- function(path,char_size=5,segment_sep=20,indent_size=50,cols){
+codeart_script <- function(path,char_size=5,segment_sep=20,indent_size=50,cols=NULL){
+  if(is.null(cols)){
+    col_palette <- c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
+                     "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
+                     "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
+                     "#8A7C64", "#599861")
+
+    cols <- sample(col_palette,14)
+
+  }
+
+  names(cols) <- c("COMMENT", "SYMBOL_FUNCTION_CALL", "SYMBOL", "FOR", "IF", "ELSE IF",
+                   "ELSE", "ASSIGNBLOCK", "FORBLOCK", "IFBLOCK", "NUM_CONST", "STR_CONST",
+                   "PIPE", "PARAMBLOCK")
+
   script <- readLines(path)
 
   parsed_script <- utils::getParseData((parse(text=script)))
--- R/plot.R
+++ R/plot.R
@@ -13,7 +13,45 @@ plot_codeart_rand <- function(df,seg_size=3,bg_col="grey25"){
     ggplot2::geom_segment(ggplot2::aes_(x=~x,y=~y,xend=~xend,yend=~yend),
                           col=df$data$col,size=seg_size,lineend = "round")+
     ggplot2::theme_void()+
-    ggplot2::theme(panel.background = ggplot2::element_rect(fill=bg_col))+
+    ggplot2::theme(
+      plot.background = ggplot2::element_rect(fill=bg_col),
+      panel.background = ggplot2::element_rect(fill=bg_col))+
     ggplot2::scale_x_continuous(limits=c(0,df$params$w))+
     ggplot2::scale_y_reverse(limits=c(df$params$h,0))
+}
+
+
+#' @title Plot Code Art generated from an R script
+#' @param df result from running [codeart_script]
+#' @param seg_size width of code segments
+#' @param bg_col background color
+#' @param pipe_size size of pipes
+#' @param caption caption
+#' @param title title
+#' @param title_size size of title
+#' @param caption_size size of caption
+#' @param font font family
+#' @author David Schoch
+#' @export
+
+plot_codeart_script <- function(df,seg_size=3,bg_col="grey25",
+                                pipe_size=8,caption="",title="",
+                                title_size=62,caption_size=32,font="Arial"){
+  if(!requireNamespace("ggplot2", quietly = TRUE)){
+    stop("The package 'ggplot2' is needed for this function.")
+  }
+  ggplot2::ggplot()+
+    ggplot2::geom_segment(data=df[df$lines!="PIPE",],
+                          ggplot2::aes_(y=~y,yend=~yend,x=~x,xend=~xend),size=seg_size,
+                 lineend = "round",col=df$col[df$lines!="PIPE"])+
+    ggplot2::geom_point(data=df[df$lines=="PIPE",],ggplot2::aes_(x=~x,y=~y),shape="\u25B6",
+               size=pipe_size,col=df$col[df$lines=="PIPE"])+
+    ggplot2::scale_y_reverse(limits=c(max(df$y),0))+
+    ggplot2::theme_void()+
+    ggplot2::labs(title=title,caption=caption)+
+    ggplot2::theme(
+      plot.background = ggplot2::element_rect(fill=bg_col),
+      panel.background = ggplot2::element_rect(fill=bg_col),
+      plot.title = ggplot2::element_text(colour="white",family=font,size=title_size,hjust=1),
+      plot.caption = ggplot2::element_text(colour="white",family=font,size=caption_size))
 }
